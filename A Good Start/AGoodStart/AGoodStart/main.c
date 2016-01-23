0128b0b5c236b6d635b06db4bdb390c19d16bf39
BenjaminABurgess <anathemus@gmail.com>
2016-01-22 20:00:59 -0500

    A Good Start
    first exercise

---
+++ A Good Start/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Benjamin A Burgess on 1/22/16.
+//  Copyright © 2016 Benjamin A Burgess. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    
+    printf("It was the worst of times.\n");
+    
+    /* Is that actually any good?
+     Maybe it needs a rewrite. */
+    
+    return 0;
+}
